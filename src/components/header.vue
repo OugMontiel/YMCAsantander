2a93a588627e6352daf5a0ba919efb456e7c9d0c
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-03 18:54:51 -0500

    feat: :construction: Comoponte header aun en costruccion
    EL diseño de la paguina aun es un miestrio

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,14 +1,23 @@
 <script>
+import logoYMCA from "@/assets/logo.webp";
+
 export default {
   name: "Header",
   data() {
     return {
       isMenuOpen: false,
+      logoYMCA,
     };
   },
   methods: {
-    toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen;
+    // En lugar de usar querySelector
+    methods: {
+      toggleMenu() {
+        this.isMenuOpen = !this.isMenuOpen;
+      },
+      closeMenu() {
+        this.isMenuOpen = false;
+      },
     },
   },
 };
@@ -16,37 +25,43 @@ export default {
 
 <template>
   <header>
-    <div class="logo">
-      <img src="../assets/logo.png" alt="Logo" />
-    </div>
-    <div class="logoChico">
-      <img src="../assets/logo.png" alt="Logo" />
-    </div>
-    
-    <nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-        <li><router-link to="/projects">Projects</router-link></li>
+    <!-- seccion de logos, uno grande y otro chico para diferenciar pantallas -->
+    <router-link to="/" class="logoLink">
+      <picture>
+        <img :src="logoYMCA" alt="Logo" class="logo" loading="lazy" />
+        <img :src="logoYMCA" alt="Logo" class="logoChico" loading="lazy" />
+      </picture>
+    </router-link>
+
+    <!-- seccion de nevacion por la pagina -->
+    <nav class="navHeader" aria-label="Navegación principal">
+      <ul class="navList" :class="{show: isMenuOpen}" @click="closeMenu">
+        <li role="none"><router-link to="/" role="menuitem" class="navLink">Inicio</router-link></li>
+        <li role="none"><router-link to="/about" role="menuitem" class="navLink">¿quienes Somos?</router-link></li>
+        <li role="none"><router-link to="/Programas" role="menuitem" class="navLink">Programas</router-link></li>
+        <li role="none"><router-link to="/Voluntariado" role="menuitem" class="navLink">Voluntariado</router-link></li>
+        <li role="none"><router-link to="/Publicaciones" role="menuitem" class="navLink">Publicaciones</router-link></li>
       </ul>
     </nav>
 
+    <!-- seccion de iconos para cuando la pntalla es pequeña -->
+    <button class="menuIcono" @click="toggleMenu" :arial-expanded="isMenuOpen" aria-label="Toggle navigation menu">
+      <UIcon :name="isMenuOpen ? 'material-symbols-light:close' : 'material-symbols-light:menu-rounded'" class="size-10" />
+    </button>
     <div class="menu-icon" @click="toggleMenu">
-      <i class="fas fa-bars"></i>
+      <UButton icon="material-symbols-light:menu-rounded" variant="subtle">Button</UButton>
     </div>
-    
   </header>
 </template>
+
 <style>
 header {
+  height: 4em;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 1em 2em;
-}
-
-nav {
-  flex:1;
+  background-color: #333;
 }
 
 nav ul {
@@ -59,5 +74,39 @@ nav ul {
 nav ul li {
   margin: 0 1em;
 }
-</style>
 
+.menu-icon {
+  display: none; /* Ocultar el ícono del menú por defecto */
+}
+.logoChico {
+  display: none; /* Mostrar el logo grande */
+}
+
+img {
+  width: 8em;
+}
+
+/* Estilos para pantallas pequeñas (celulares) */
+@media (max-width: 480px) {
+  .logo {
+    display: none; /* Ocultar el logo grande */
+  }
+
+  .logoChico {
+    display: block; /* Mostrar el logo chico */
+  }
+
+  nav ul {
+    flex-direction: column;
+    display: none; /* Ocultar el menú por defecto */
+  }
+
+  nav ul.show {
+    display: flex; /* Mostrar el menú cuando se active */
+  }
+
+  .menu-icon {
+    display: block; /* Mostrar el ícono del menú */
+  }
+}
+</style>
