5c6871bbca6ae8cf69232fac8f641c1a068484c6
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-03 15:33:19 -0500

    feat: :tada: Inicio de proyecto
    Lanzamiento de vue.js y la respectiva eliminacion de los elementos extras

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="../assets/logo.png" alt="Logo" />
+    </div>
+    
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/projects">Projects</router-link></li>
+      </ul>
+    </nav>
+    
+  </header>
+</template>
