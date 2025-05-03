c409ec368f2ae70792c8530059f43534932c88b3
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-03 16:28:23 -0500

    feat: :construction: contendio del header y del app
    se trabaja en el header

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <script setup>
-import {RouterView} from "vue-router";
+import { RouterView } from "vue-router";
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 
 <template>
-  <Header />
-  <main>
-    <RouterView />
-  </main>
-  <Footer />
+  <div class="app-container">
+    <Header />
+    <main class="main-content">
+      <RouterView />
+    </main>
+    <Footer />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  flex: 1;
+  padding: 1rem;
+}
+</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,25 @@
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
+</script>
+
 <template>
   <header>
     <div class="logo">
+      <img src="../assets/logo.png" alt="Logo" />
+    </div>
+    <div class="logoChico">
       <img src="../assets/logo.png" alt="Logo" />
     </div>
     
@@ -11,6 +30,34 @@
         <li><router-link to="/projects">Projects</router-link></li>
       </ul>
     </nav>
+
+    <div class="menu-icon" @click="toggleMenu">
+      <i class="fas fa-bars"></i>
+    </div>
     
   </header>
 </template>
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em 2em;
+}
+
+nav {
+  flex:1;
+}
+
+nav ul {
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+nav ul li {
+  margin: 0 1em;
+}
+</style>
+
