0210726d65e717c0838381a921d29a38225c42ff
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-11 06:36:26 -0500

    feat: :lipstick: incoporacion de logo
    Se incorporo logo

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,6 +1,6 @@
 <script>
-import logoChico from "@/assets/logos/Logo_Ymca-removebg-preview.png";
-import logoGrande from "@/assets/logos/logo.webp";
+import logoChico from "@/assets/logos/LogoAzul.png";
+import logoGrande from "@/assets/logos/LogoAzul.png";
 
 export default {
   name: "Header",
