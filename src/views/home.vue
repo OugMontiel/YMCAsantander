0b2cc28c13a72f727f2e7c63ea10572096786cea
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-13 11:24:55 -0500

    feat: :art: carusel
    Falta img reales para poder hacer una buena adecuacion

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,3 +1,33 @@
+<script setup lang="js">
+import img1 from "@/assets/fondos/fondoAzulOpc1.png";
+import img2 from "@/assets/fondos/fondoMoradoOpc1.png";
+import img3 from "@/assets/fondos/fondoNaranjaOpc1.png";
+import img4 from "@/assets/fondos/fondoAzulOpc1.png";
+import img5 from "@/assets/fondos/fondoMoradoOpc1.png";
+import img6 from "@/assets/fondos/fondoNaranjaOpc1.png";
+
+const items = [img1, img2, img3, img4, img5, img6];
+</script>
+
 <template>
-  <main>Hola mundo</main>
+  <div class="home">
+    <UCarousel
+      v-slot="{item}"
+      loop
+      arrows
+      auto-scroll
+      :items="items"
+      :ui="{
+        item: 'basis-1/3 shrink-0',
+      }"
+    >
+      <img :src="item" />
+    </UCarousel>
+  </div>
 </template>
+
+<style scoped>
+.home img {
+  height: 40em;
+}
+</style>
