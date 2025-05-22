39d3a415541303def0b603bf3f664f1f822b8514
OugMontiel <gogoedy@hotmail.com>
2025-05-22 13:46:25 -0500

    refactor: :rotating_light: Se genero una vercion Util para publicar
    Vamos a ver como funciona y si tiene errore

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,15 @@
 import {createRouter, createWebHistory} from "vue-router";
+import EnConstruccion from "../components/util/EnConstruccion.vue"; // nuevo componente genérico
 
 // vistas principales
 import Home from "../views/home.vue";
 import About from "../views/about.vue";
 
 // componentes de barra de navegacion de las rutas madres
-import AporteHeader from "../components/header/aporteHeader.vue";
-import ExperienciaHeader from "../components/header/experienciasHeader.vue";
-import ProyectosHeader from "../components/header/proyectosHeader.vue";
-import SocialHeader from "../components/header/socialHeader.vue";
 import LegalHeader from "../components/header/legalHeader.vue";
 
 // vistas legales
 import Esal from "../views/legal/Esal.vue"; // Inicio de la vista legal
-import Privacidad from "../views/legal/Privacidad.vue";
-import TerminosYCondiciones from "../views/legal/TerminosYCondiciones.vue";
-import PoliticaDeCookies from "../views/legal/PoliticaDeCookies.vue";
-
-// Vistas Proyectos
-import Proyectos from "../views/proyectos/proyectos.vue";
-import PazaLaPaz from "../views/proyectos/pazaLaPaz.vue";
-import MisionPazYCiudadania from "../views/proyectos/misionPazYCiudadania.vue";
-
-// vistas sociales
-import Social from "../views/social/social.vue";
-import Publicaciones from "../views/social/publicaciones.vue";
-import Voluntariado from "../views/social/voluntariado.vue";
-
-// vistas de aportes
-import Socio from "../views/aporta/socio.vue"; // Inicio de la vista de aportes
-import Donar from "../views/aporta/donar.vue";
-import HazteVoluntario from "../views/aporta/hazteVoluntario.vue";
-
-// vistas de experiencia
-import Experiencia from "../views/experiencias/experiencia.vue";
-import AreaInternacional from "../views/experiencias/areaInternacional.vue";
-import experienciasLocales from "../views/experiencias/experienciasLocales.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,99 +25,6 @@ const router = createRouter({
       component: About,
     },
     {
-      path: "/experiencia",
-      name: "experiencia",
-      component: ExperienciaHeader,
-      children: [
-        {
-          path: "",
-          name: "experienciaInicio",
-          component: Experiencia,
-        },
-        {
-          path: "areaInternacional",
-          name: "areaInternacional",
-          component: AreaInternacional,
-        },
-        {
-          path: "experienciasLocales",
-          name: "experienciasLocales",
-          component: experienciasLocales,
-        },
-      ],
-    },
-    {
-      path: "/proyectos",
-      name: "proyectos",
-      component: ProyectosHeader,
-      children: [
-        {
-          path: "",
-          name: "ProyectosInicio",
-          component: Proyectos,
-        },
-        {
-          path: "pazaLaPaz",
-          name: "pazaLaPaz",
-          component: PazaLaPaz,
-        },
-        {
-          path: "misionPazYCiudadania",
-          name: "misionPazYCiudadania",
-          component: MisionPazYCiudadania,
-        },
-      ],
-    },
-    {
-      path: "/aporta",
-      name: "aporta",
-      component: AporteHeader,
-      children: [
-        {
-          path: "",
-          name: "aportaInicio",
-          component: Socio,
-        },
-        {
-          path: "Socio",
-          name: "Socio",
-          component: Socio,
-        },
-        {
-          path: "donar",
-          name: "donar",
-          component: Donar,
-        },
-        {
-          path: "hazteVoluntario",
-          name: "hazteVoluntario",
-          component: HazteVoluntario,
-        },
-      ],
-    },
-    {
-      path: "/social",
-      name: "social",
-      component: SocialHeader,
-      children: [
-        {
-          path: "",
-          name: "socialInicio",
-          component: Social,
-        },
-        {
-          path: "voluntariado",
-          name: "voluntariado",
-          component: Voluntariado,
-        },
-        {
-          path: "publicaciones",
-          name: "publicaciones",
-          component: Publicaciones,
-        },
-      ],
-    },
-    {
       path: "/Esal",
       name: "vistasLegales",
       component: LegalHeader,
@@ -153,22 +34,14 @@ const router = createRouter({
           name: "esal",
           component: Esal,
         },
-        {
-          path: "Privacidad",
-          name: "Privacidad",
-          component: Privacidad,
-        },
-        {
-          path: "terminosYCondiciones",
-          name: "terminosYCondiciones",
-          component: TerminosYCondiciones,
-        },
-        {
-          path: "politicaDeCookies",
-          name: "politicaDeCookies",
-          component: PoliticaDeCookies,
-        },
       ],
+    },
+
+    // 🚧 CUALQUIER OTRA RUTA que existía antes, ahora muestra "En Construcción"
+    {
+      path: "/:catchAll(.*)",
+      name: "enConstruccion",
+      component: EnConstruccion,
     },
   ],
 });
