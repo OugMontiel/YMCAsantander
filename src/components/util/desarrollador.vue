ee33a356095975f424bcaee964630c2c36485473
PC_Monther <88790286+OugMontiel@users.noreply.github.com>
2025-05-23 12:04:49 -0500

    fix: :construction: arreglo de footer
    que en md - Tablets ... para edecuar el responsive

---
+++ src/components/util/desarrollador.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footerDesarrollador">
+    <p>© 2023 YMCA. Todos los derechos reservados.</p>
+    <p>
+      Desarrollado por
+      <a href="#">Diego Alejandro Montiel Flórez</a>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.footerDesarrollador {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: var(--color-texto-secundario);
+  border-top: 1px solid var(--color-borde);
+}
+@media (max-width: 670px) {
+  .footerDesarrollador {
+    flex-direction: column;
+  }
+}
+</style>
