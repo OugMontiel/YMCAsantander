d0413099a2c7f7ea9c513082b9ef6ea7b44e0537
OugMontiel <gogoedy@hotmail.com>
2025-05-29 17:15:03 -0500

    feat: :construction: Modualcion mision paz
    modulacion

---
+++ src/components/proyectos/misionPazYCiudadania/testimonios.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h3 class="testimonials-title">TESTIMONIOS</h3>
+    <p class="testimonials-content">Agregar testimonios aquí.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Testimonios'
+}
+</script>
--- src/views/proyectos/misionPazYCiudadania.vue
+++ src/views/proyectos/misionPazYCiudadania.vue
@@ -1,50 +1,24 @@
+<script>
+import MisionDescripcion from '@/components/proyectos/misionPazYCiudadania/descripcion.vue'
+import ImpactoIniciativas from '@/components/proyectos/misionPazYCiudadania/impactoIniciativas.vue'
+import Testimonios from '@/components/proyectos/misionPazYCiudadania/testimonios.vue'
+
+export default {
+  name: 'PazYCiudadania',
+  components: {
+    MisionDescripcion,
+    ImpactoIniciativas,
+    Testimonios
+  }
+}
+</script>
 <template>
   <div class="pazYCiudadania">
     <h2 class="section-title">MISIÓN, PAZ y CIUDADANÍA</h2>
 
-    <h3 class="description-title">DESCRIPCIÓN</h3>
-    <p class="description">
-      El Proyecto <strong>Misión Paz – Líderes Juveniles Multiplicadores de Paz</strong> tiene el objetivo de “Fortalecer el liderazgo
-      juvenil desarrollando competencias y habilidades en jóvenes líderes, empoderándolos como formadores y multiplicadores de Paz quienes
-      contribuyen a la transformación de sus entornos comunitarios y aportan en la construcción de paz en el departamento de Santander”.
-    </p>
-    <p>
-      Para lograrlo, se parte de la convocatoria y selección de aquellos jóvenes líderes con el perfil y el interés manifiesto de
-      proyectarse como agentes de cambio, fortaleciendo sus conocimientos, habilidades y competencias en construcción de paz, prevención,
-      resolución de conflictos, transferencia de saberes y afianzamiento de prácticas.
-    </p>
-    <p>
-      Este proyecto es financiado desde la <strong>Fundación Bolívar Davivienda</strong> y busca impactar de manera directa a
-      <strong>100 jóvenes</strong>, los cuales son formados como líderes multiplicadores de paz, con una labor importante de generar
-      iniciativas sociales que contribuyan a la transformación social de las comunidades.
-    </p>
-
-    <h3 class="impact-title">IMPACTO</h3>
-    <p>
-      Este proyecto se ha ejecutado en dos ocasiones, y en su última versión, se logró la realización de
-      <strong>19 iniciativas de incidencia social</strong>, entre las cuales se pueden resaltar:
-    </p>
-
-    <ul class="impact-initiatives">
-      <li>
-        <strong>Alma Sana:</strong> es un proyecto que busca generar hábitos que incidan de manera positiva en la salud mental de los
-        voluntarios de la YMCA Santander.
-      </li>
-      <li>
-        <strong>Plantearte:</strong> es un proyecto que busca la concientización sobre el cuidado del medio ambiente, por medio de
-        actividades que involucran el arte.
-      </li>
-      <li>
-        <strong>EcoÁngulo:</strong> busca generar conciencia en los estudiantes del colegio Ángulo sobre el cuidado del medio ambiente, a
-        partir del aprovechamiento de los recursos que se pueden reciclar.
-      </li>
-      <li>
-        <strong>Hora del Café:</strong> busca concientizar a las mujeres de la comunidad del barrio El Cinal del municipio de Bucaramanga,
-        sobre la importancia de incentivar el autocuidado, para fortalecer su rol de mujer.
-      </li>
-    </ul>
-
-    <h3 class="testimonials-title">TESTIMONIOS</h3>
-    <p class="testimonials-content">Agregar testimonios aquí.</p>
+    <MisionDescripcion />
+    <ImpactoIniciativas />
+    <Testimonios />
   </div>
 </template>
+
