64f40fde72249cb43d5d4bebdf2430c8bf8e4c55
PcGamer <gogoedy@hotmail.com>
2025-07-27 10:46:46 -0500

    Updates

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker para deshabilitar cache
+
+self.addEventListener('install', event => {
+    // No se cachea nada
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    // Desregistrar este Service Worker para deshabilitar cache
+    event.waitUntil(
+        self.registration.unregister()
+        .then(() => {
+            return self.clients.matchAll()
+            .then(clients => {
+                clients.forEach(client => client.navigate(client.url));
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    // No se utiliza caché, simplemente forwardear la petición
+    event.respondWith(fetch(event.request));
+});
+
