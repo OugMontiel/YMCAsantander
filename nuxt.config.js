c804d03aa83f8507ba100402c392a59acf4bd834
PcGamer <gogoedy@hotmail.com>
2025-09-15 21:18:31 -0500

    feat: add sitemap module and configuration
    - Added @nuxtjs/sitemap module to the project dependencies.
    - Configured sitemap settings in nuxt.config.js with specified hostname and routes.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,20 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxtjs/sitemap',
   ],
   ui: {
     icons: true
+  },
+  sitemap: {
+    hostname: 'https://www.ymcasantander.org',
+    gzip: true,
+    routes: [
+      '/about',
+      '/proyectos',
+      '/legal/esal',
+      '/legal/privacidad',
+      '/legal/terminos-y-condiciones'
+    ]
   }
 })
